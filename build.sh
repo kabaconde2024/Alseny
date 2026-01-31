34d335318714356c1c646bb0044bbecf79d035df
kabaconde2024 <kabaconde5259@gmail.com>
2026-01-31 18:03:46 +0100

    Initialisation du projet Laravel pour déploiement

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Installation des dépendances
 composer install --no-dev --optimize-autoloader
 npm install
 npm run build
-php artisan migrate --forc
+
+# Nettoyage COMPLET des caches pour forcer Brevo
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
+# Migration de la base de données
+php artisan migrate --force
+
+# Mise en cache propre (Optionnel mais recommandé pour la vitesse)
+php artisan config:cache
